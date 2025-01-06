5f53d1786994c284a33ec51e7dffed063a5fca67
cassiaaaaa <cassia.tay.2024@computing.smu.edu.sg>
2025-01-06 19:16:30 +0800

    added exercises for mock

--- src/shopping_cart.py
+++ src/shopping_cart.py
@@ -1,16 +1,38 @@
-def add_item(cart, item, quantity):
+import random
+
+def add_item(cart, item, quantity, price_map):
     if quantity <= 0:
         raise ValueError("Quantity must be greater than zero")
+    if item not in price_map:
+        raise KeyError("Item does not exist, ask a staff")
     cart.append({'item': item, 'quantity': quantity})
 
 
 def remove_item(cart, item):
-    return [i for i in cart if i['item'] != item]
+    cart.remove(item)
+    return cart
 
 
 def total_items(cart):
-    return sum(i['quantity'] for i in cart)
+    total_items = 0
+    for item in cart:
+        total_items += item["quantity"]
+    return total_items
 
 
 def is_empty(cart):
     return len(cart) == 0
+
+
+def total_price(cart, price_map):
+    total_price = 0
+    for item in cart:
+        total_price += price_map[item]*item["quantity"]
+    return total_price
+
+
+def get_discounted_price(cart, price_map):
+	lucky_number = random.randint(1, 2000)
+	if lucky_number == 7:
+		discounted_price = total_price(cart, price_map) * 0.1
+		return round(discounted_price, 2)
--- test/test_shopping_cart.py
+++ test/test_shopping_cart.py
@@ -1,5 +1,6 @@
 import pytest
-import shopping_cart as sc
+import src.shopping_cart as sc
+from unittest.mock import patch
 
 # Fixtures
 @pytest.fixture
@@ -10,17 +11,21 @@ def empty_cart():
 def filled_cart():
     return [{'item': 'apple', 'quantity': 2}, {'item': 'banana', 'quantity': 3}]
 
+@pytest.fixture
+def price_map():
+    return {'apple': 1, 'orange': 2, 'banana': 3, 'cherry': 4}
+
 # Tests
 @pytest.mark.smoke
 def test_empty_cart_is_empty(empty_cart):
     assert sc.is_empty(empty_cart)
 
 @pytest.mark.regression
-def test_add_item_to_cart(empty_cart):
-    sc.add_item(empty_cart, 'orange', 1)
+def test_add_item_to_cart(empty_cart, price_map):
+    sc.add_item(empty_cart, 'orange', 1, price_map)
     assert not sc.is_empty(empty_cart)
 
-@pytest.mark.slow
+@pytest.mark.slows
 def test_total_items_in_cart(filled_cart):
     assert sc.total_items(filled_cart) == 5
 
@@ -32,8 +37,8 @@ def test_total_items_in_cart(filled_cart):
         ('cherry', 3, 3)
     ]
 )
-def test_add_multiple_items(empty_cart, item, quantity, expected):
-    sc.add_item(empty_cart, item, quantity)
+def test_add_multiple_items(empty_cart, item, quantity, expected, price_map):
+    sc.add_item(empty_cart, item, quantity, price_map)
     assert sc.total_items(empty_cart) == expected
 
 @pytest.mark.skip(reason="Remove functionality not yet implemented")
@@ -46,9 +51,19 @@ def test_skipif_example():
     assert True
 
 @pytest.mark.xfail(raises=ValueError)
-def test_add_invalid_quantity(empty_cart):
-    sc.add_item(empty_cart, 'pear', 0)
+def test_add_invalid_quantity(empty_cart, price_map):
+    sc.add_item(empty_cart, 'pear', 0, price_map)
 
 @pytest.mark.usefixtures("filled_cart")
 def test_cart_not_empty(filled_cart):
     assert not sc.is_empty(filled_cart)
+
+
+@patch("random.randint")
+@patch("src.shopping_cart.total_price")
+def test_get_discounted_price(mock_total_price, mock_randint, filled_cart, price_map):
+	mock_randint.return_value = 7
+	mock_total_price.return_value = (2*1 + 3*3)
+	
+	discounted_price = sc.get_discounted_price(filled_cart, price_map)
+	assert discounted_price == round((2*1 + 3*3)*0.1, 2)
